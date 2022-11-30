649b6f7db7378b4b43b03bbea5aca1549191d824
bruno ginél <bruno_bginel@hotmail.com>
2022-11-30 16:37:51 -0300

    secund commit

--- interacao.js
+++ interacao.js
@@ -1,17 +1,17 @@
-let btnOpen = document.querySelector('.btn-open')
-let btnClose =document.querySelector('.btn-close')
-let listNav = document.querySelector('.nav')
-let listLogin = document.querySelector('.login')
-let bgNav = document.querySelector('div.bg-nav')
+let btnOpen = document.querySelector("button.btn-open");
+let btnClose = document.querySelector("button.btn-close");
+let listNav = document.querySelector(".nav");
+let listLogin = document.querySelector(".login");
+let bgNav = document.querySelector("div.bg-nav");
 
 function OpenNav() {
   bgNav.classList.add("active");
-  btnOpen.style.display = 'none'
-  btnClose.style.display = 'block'
+  btnOpen.style.display = "none";
+  btnClose.style.display = "block";
 }
 
 function CloseNav() {
-  bgNav.classList.remove("active")
-  btnClose.style.display = 'none'
-  btnOpen.style.display = ''
+  bgNav.classList.remove("active");
+  btnClose.style.display = "none";
+  btnOpen.style.display = "block";
 }
